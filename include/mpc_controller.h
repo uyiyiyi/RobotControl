3f6b79c91d5ee01c2d420a17263fe5c583e24969
uyiyiyi <670434852@qq.com>
2024-10-29 15:20:23 +0800

    MPC调试完成

--- include/mpc_controller.h
+++ include/mpc_controller.h
@@ -9,6 +9,7 @@
 #include "DiffDriveModel.h"
 
 using namespace Eigen;
+using namespace std;
 
 MatrixXd MatrixPower(MatrixXd Mat, int n)
 {
@@ -24,26 +25,30 @@ class mpc_controller
 {
 private:
     /* data */
-    double dt = 0.1; // 时间步长
-    int N = 10;     // 预测步长
-    MatrixXd A, B, A_big, B_big, Q, R, Q_big, R_big, H, f;
-    int state_dim = 3;
-    int control_dim = 2;
+    double dt; // 时间步长
+    int N;     // 预测步长
+    int state_dim;
+    int control_dim;
 public:
-    mpc_controller(/* args */);
+    MatrixXd A, B, A_big, B_big, Q, R, Q_big, R_big, H, f;
+    mpc_controller(double T, int n, int state, int control);
     ~mpc_controller();
     void calABQRHf(const std::vector<double>& v_theta, const Eigen::VectorXd& X_0, const Eigen::VectorXd& X_ref);
     VectorXd solveQP();
 };
 
-mpc_controller::mpc_controller(/* args */)
+mpc_controller::mpc_controller(double T, int n, int state, int control)
 {
+    dt = T;
+    N = n;
+    state_dim = state;
+    control_dim = control;
     A = MatrixXd::Zero(state_dim, state_dim);
     B = MatrixXd::Zero(state_dim, control_dim);
     A_big = MatrixXd::Zero(state_dim * N, state_dim);
     B_big = MatrixXd::Zero(state_dim * N, control_dim * N);
-    Q = MatrixXd::Zero(state_dim, state_dim);
-    R = MatrixXd::Zero(control_dim, control_dim);
+    Q = MatrixXd::Identity(state_dim, state_dim);
+    R = MatrixXd::Identity(control_dim, control_dim);
     Q_big = MatrixXd::Zero(state_dim * N, state_dim * N);
     R_big = MatrixXd::Zero(control_dim * N, control_dim * N);
     H = MatrixXd::Zero(control_dim * N, control_dim * N);
@@ -60,11 +65,13 @@ void mpc_controller::calABQRHf(const std::vector<double>& v_theta, const Eigen::
     // 计算 A, B, Q, R
     A << 1, 0, -dt * v_theta[0] * sin(v_theta[1]), 0, 1, dt * v_theta[0] * cos(v_theta[1]), 0, 0, 1;
     B << dt * cos(v_theta[1]), 0, dt * sin(v_theta[1]), 0, 0, dt;
+    cout << "A is: " << A << endl;
+    cout << "B is: " << B << endl;
     for (size_t i = 0; i < N; i++)
     {
         A_big.block(i * state_dim, 0, state_dim, state_dim) = MatrixPower(A, i+1);
-        Q_big.block(i * state_dim, i * state_dim, state_dim, state_dim) = Q;
-        R_big.block(i * control_dim, i * control_dim, control_dim, control_dim) = R;
+        Q_big.block(i * state_dim, i * state_dim, state_dim, state_dim) = 10 * Q;
+        R_big.block(i * control_dim, i * control_dim, control_dim, control_dim) = 0.1 * R;
     }
     for (size_t i = 0; i < N; i++)
     {
@@ -73,26 +80,45 @@ void mpc_controller::calABQRHf(const std::vector<double>& v_theta, const Eigen::
             B_big.block(i * state_dim, j * control_dim, state_dim, control_dim) = MatrixPower(A, i - j) * B;
         }
     }
+    cout << "A_big is: " << A_big << endl;
+    cout << "Q_big is: " << Q_big << endl;
+    cout << "R_big is: " << R_big << endl;
+    cout << "B_big is: " << B_big << endl;
     // 计算 H 和 f
-    Eigen::MatrixXd H = B_big.transpose() * Q_big * Q_big + R_big;
-    Eigen::VectorXd f = B_big.transpose() * Q_big * (A_big * X_0 - X_ref);
+    cout << "B_big shape: " << B_big.rows() << " * " << B_big.cols() << endl;
+    cout << "Q_big shape: " << Q_big.rows() << " * " << Q_big.cols() << endl;
+    cout << "R_big shape: " << R_big.rows() << " * " << R_big.cols() << endl;
+    cout << "A_big shape: " << A_big.rows() << " * " << A_big.cols() << endl;
+    cout << "X_0 shape: " << X_0.rows() << " * " << X_0.cols() << endl;
+    cout << "X_ref shape: " << X_ref.rows() << " * " << X_ref.cols() << endl;
+    cout << "X_ref is: " << X_ref << endl;
+    H = B_big.transpose() * Q_big * B_big + R_big;
+    cout << "H shape: " << H.rows() << " * " << H.cols() << endl;
+    cout << "H is: " << endl << H << endl;
+    f = B_big.transpose() * Q_big * (A_big * X_0 - X_ref);
+    cout << "f shape: " << f.rows() << " * " << f.cols() << endl;
+    cout << "f is: " << endl << f << endl;
 }
 
 VectorXd mpc_controller::solveQP() 
 {
     // instantiate the solver
     OsqpEigen::Solver solver;
+    int num_constraints = 0;
 
     // settings
-    solver.settings()->setVerbosity(false);
+    solver.settings()->setVerbosity(true);
     solver.settings()->setWarmStart(true);
 
     // set the initial data of the QP solver
     solver.data()->setNumberOfVariables(control_dim * N);   //变量数
-    solver.data()->setNumberOfConstraints(control_dim * N); //约束数
+    solver.data()->setNumberOfConstraints(num_constraints); //约束数
 
+    cout << "H is: " << endl << H << endl;
     SparseMatrix<double> Hessian_sparse = H.sparseView();
     VectorXd f_ = f;
+    cout << "Hessian_sparse is: " << endl << Hessian_sparse << endl;
+    cout << "f_ is: " << endl << f_ << endl;
     
     solver.data()->setHessianMatrix(Hessian_sparse);
     solver.data()->setGradient(f_);
@@ -102,7 +128,18 @@ VectorXd mpc_controller::solveQP()
         throw std::runtime_error("OSQP initialization failed!");
     }
 
+    if(!solver.solve())
+    {
+        throw std::runtime_error("OSQP solve failed!");
+    }
+    
     VectorXd optimal_control = solver.getSolution();
+
+    cout << "_______________________________________________________________________________" << endl;
+    cout << "_______________________________________________________________________________" << endl;
+    cout << "optimal_control: " << optimal_control << endl;
+    cout << "_______________________________________________________________________________" << endl;
+    cout << "_______________________________________________________________________________" << endl;
 
     return optimal_control;
 }
--- src/MPC_Control.cpp
+++ src/MPC_Control.cpp
@@ -2,62 +2,98 @@
 #include "draw.h"
 
 
+
 int main() {
     int num_points = 100; // 生成的轨迹点数量
+    double v_max = 2;
+    double w_max = M_PI;
     // 获取圆轨迹
     std::vector<std::vector<double>> trajectory = generateCircleTrajectory(num_points);
 
+    double dt = 0.01;
+    int N = 10;
+    int state_dim = 3;
+    int control_dim = 2;
     // 初始化MPC控制器
-    mpc_controller mpc;
+    mpc_controller mpc(dt, N, state_dim, control_dim);
 
     // // 定义差分驱动机器人模型的初始状态
-    VectorXd init_state(3);
-    init_state << 0.0, 0.0, 0.0; // 初始状态 x=0, y=0, theta=0
-    VectorXd current_state, X_ref;
+    VectorXd init_state(state_dim);
+    init_state << 4.9, 0, M_PI / 2; // 初始状态 x=0, y=0, theta=0
+    VectorXd current_state(state_dim), X_ref(state_dim * N);
     double robot_x, robot_y, robot_yaw;
     std::vector<double> ctrl = {2.0, 0.0}; // 线速度和角速度
-    std::vector<double> v_theta = {0.0, 0.0};
+    std::vector<double> v_theta = {0, 0};
     
     DiffDriveModel robot(init_state); // 使用已有的差分驱动模型
-
+    size_t target_index = 0; // 当前目标点的索引
     // 控制循环
     while (true) {
-        // 获取机器人当前状态
-        robot.getState(robot_x, robot_y, robot_yaw); // 假设这个函数存在
-        current_state << robot_x, robot_y, robot_yaw;
+        if (target_index < trajectory.size() - N) {
+            std::vector<double> target_point = trajectory[target_index];
+            // 获取机器人当前状态
+            robot.getState(robot_x, robot_y, robot_yaw); // 假设这个函数存在
+            current_state << robot_x, robot_y, robot_yaw;
+            cout << "current_state: " << current_state << endl;
+            for (size_t i = 0; i < N; i++)
+            {
+                X_ref(3 * i) = trajectory[i + target_index][0];
+                X_ref(3 * i + 1) = trajectory[i + target_index][1];
+                X_ref(3 * i + 2) = trajectory[i + target_index][2];
+            }
 
-        mpc.calABQRHf(v_theta, current_state, X_ref);
-        // 求解MPC问题，获取控制输入
-        VectorXd control_input = mpc.solveQP();
+            v_theta[0] = ctrl[0];
+            v_theta[1] = robot_yaw;
+            mpc.calABQRHf(v_theta, current_state, X_ref);
+            // 求解MPC问题，获取控制输入
+            VectorXd optimal_control = mpc.solveQP();
+            
 
-        // 使用控制输入更新机器人状态
-        robot.updateState(control_input(0), control_input(1)); // 假设此函数更新机器人状态
+            // 更新控制指令
+            ctrl[0] = optimal_control(0);
+            ctrl[1] = optimal_control(1);
+            ctrl[0] = clamp(ctrl[0], -v_max, v_max);
+            ctrl[1] = clamp(ctrl[1], -w_max, w_max);
+            
+            // 使用控制输入更新机器人状态
+            robot.updateState(ctrl[0], ctrl[1]); // 假设此函数更新机器人状态
+            // 检查是否到达目标点
+            double distance_to_target = std::hypot(target_point[0] - robot_x, target_point[1] - robot_y);
+            cout << "distance_to_target = " << distance_to_target << endl;
+            cout << "Target Point Index: " << target_index << endl;
+            if (distance_to_target < 0.4) { // 如果距离目标点小于阈值，切换到下一个目标点
+                target_index++;
+            }
 
-        // 输出当前状态
-        std::cout << "Current State: " << current_state.transpose() << std::endl;
+            // 输出当前状态
+            std::cout << "Current State: " << current_state.transpose() << std::endl;
 
-        // 输出当前状态和控制指令
-        std::cout << "Current state: x = " << robot_x
-                << ", y = " << robot_y
-                << ", theta = " << robot_yaw << std::endl;
-        std::cout << "Control: v = " << ctrl[0] << ", w = " << ctrl[1] << std::endl;
+            // 输出当前状态和控制指令
+            std::cout << "Current state: x = " << robot_x
+                    << ", y = " << robot_y
+                    << ", theta = " << robot_yaw << std::endl;
+            std::cout << "Control: v = " << ctrl[0] << ", w = " << ctrl[1] << std::endl;
 
-        // 计算箭头的终点坐标
-        double arrow_length = 0.1; // 箭头长度
-        double arrow_x = robot_x + arrow_length * cos(robot_yaw);
-        double arrow_y = robot_y + arrow_length * sin(robot_yaw);
+            // 计算箭头的终点坐标
+            double arrow_length = 0.1; // 箭头长度
+            double arrow_x = robot_x + arrow_length * cos(robot_yaw);
+            double arrow_y = robot_y + arrow_length * sin(robot_yaw);
 
-        // 绘制机器人位置和目标点
-        plt::clf(); // 清除上一次绘图
-        draw_trajectory(trajectory); // 再次绘制轨迹
-        draw_robot(robot_x, robot_y, robot_yaw); // 更新机器人位置
+            // 绘制机器人位置和目标点
+            plt::clf(); // 清除上一次绘图
+            draw_trajectory(trajectory); // 再次绘制轨迹
+            draw_robot(robot_x, robot_y, robot_yaw); // 更新机器人位置
 
-        plt::xlim(-10, 10);
-        plt::ylim(-10, 10);
-        // 绘制表示航向的箭头
-        plt::pause(0.01); // 暂停以更新图形
-        // 模拟延时
-        std::this_thread::sleep_for(std::chrono::milliseconds(10)); // 10ms 延时模拟实时性
+            plt::xlim(-10, 10);
+            plt::ylim(-10, 10);
+            // 绘制表示航向的箭头
+            plt::pause(0.01); // 暂停以更新图形
+            // 模拟延时
+            std::this_thread::sleep_for(std::chrono::milliseconds(10)); // 10ms 延时模拟实时性
+        } else {
+            std::cout << "已到达所有目标点，停止控制." << std::endl;
+            break; // 如果已到达所有目标点，退出循环
+        }
     }
 
     return 0;
